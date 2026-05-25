9c2a2a451260f94877f55d5104baedef764c180c
Anil Kumar <anilkumarsikwal25@gmail.com>
2026-05-25 15:59:37 +0530

    update packages

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
-const API_BASE = 'http://192.168.1.9:8000/api';
+//const API_BASE = 'http://192.168.1.9:8000/api';
 
-//const API_BASE = 'https://khata-flow-api.onrender.com/api';
+const API_BASE = 'https://khata-flow-api.onrender.com/api';
 
 export interface ApiResponse<T = any> {
   success: boolean;
